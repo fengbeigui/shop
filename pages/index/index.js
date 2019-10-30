3f7e9f9737fa1cbbc0abcb78b020a5530d4bd5f7
fengbeigui <fbg0618@163.com>
2019-10-30 12:09:21 +0800

    首页搜索和轮播图静态数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,13 +3,25 @@ import request from "../../utils/request.js"
 //获取应用实例
 Page({
 
-  onLoad(){
+  data: {
+    banners: [
+      "https://cdn.pinduoduo.com/home/static/img/subject/girlclothes.jpg",
+      "https://cdn.pinduoduo.com/home/static/img/subject/boyshirt.jpg",
+      "https://cdn.pinduoduo.com/home/static/img/subject/medical.jpg"
+    ]
+  },
+
+  onLoad() {
 
     request({
-      url:"/api/public/vl/home/swiperdata"
-    }).then(res=>{
+      url: "/api/public/vl/home/swiperdata"
+    }).then(res => {
       console.log(res)
     })
-  }
-  
+  },
+
+  //轮播图
+
+  /* */
+
 })
