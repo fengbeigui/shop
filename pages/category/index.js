5f1110e8377abdbba85a932fddcf1a8281f32115
fengbeigui <fbg0618@163.com>
2019-10-30 21:18:24 +0800

    分类的左侧菜单布局&&左侧菜单的高亮

--- pages/category/index.js
+++ pages/category/index.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      //当前的初始数据
+      current:0,
   },
 
   /**
@@ -14,53 +15,16 @@ Page({
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //点击左侧的菜单时候触发
+  handleChange(event){
+    //获取index,,event.target事件目标
+    const {index} = event.target.dataset;
+    console.log(index,'点击打印索引值')
+    //打印data数据
+    this.setData({
+      current:index
+    })
   }
+
+
 })
