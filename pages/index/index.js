074077689d25e16cabe0a524b6c14fd23fd38592
fengbeigui <fbg0618@163.com>
2019-10-30 12:12:35 +0800

    菜单列表静态样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,6 +8,12 @@ Page({
       "https://cdn.pinduoduo.com/home/static/img/subject/girlclothes.jpg",
       "https://cdn.pinduoduo.com/home/static/img/subject/boyshirt.jpg",
       "https://cdn.pinduoduo.com/home/static/img/subject/medical.jpg"
+    ],
+    menus: [
+      "../../images/cart_empty@2x.png",
+      "../../images/cart_empty@2x.png",
+      "../../images/cart_empty@2x.png",
+      "../../images/cart_empty@2x.png",
     ]
   },
 
