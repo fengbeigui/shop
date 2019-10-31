15e78a9aab998e2bd2d58f47ebbf177c9cf830bf
fengbeigui <fbg0618@163.com>
2019-10-31 22:20:05 +0800

    渲染详情页数据

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,18 +1,37 @@
 // pages/goods_detail/index.js
+import request from "../../utils/request.js"
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    //detail是商品详情,对象
+    detail:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 获取id
+    const { goods_id } = options;
+     // 请求商品详情
+    request({
+      url:"/api/public/v1/goods/detail",
+      data:{
+        goods_id
+      }
+    }).then(res=>{
+      console.log(res,'555')
+      const{message} = res.data;
 
+      //detail是商品详情
+      this.setData({
+        detail: message
+      })
+    })
   },
 
   /**
