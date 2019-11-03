b503f4a3cbc2df7f0beb322af793dfdcdf3753d6
fengbeigui <fbg0618@163.com>
2019-11-04 01:55:10 +0800

    实现数量减一

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -8,7 +8,7 @@ Page({
     //收货地址
     address: {},
     //购物车商品列表
-    goods:null
+    goods: null
   },
   //获取收货地址
   // 获取收货地址
@@ -27,31 +27,72 @@ Page({
       }
     })
   },
-  onShow(){
+  onShow() {
     //每次打开页面时候都在本地获取购物车的数据
     //这方法是开微信官方文档获取
     const goods = wx.getStorageSync("goods") || null;
 
-    console.log(goods,'打印本地获取购物车的数据')
+    console.log(goods, '打印本地获取购物车的数据')
     this.setData({
       goods
     })
   },
-//数量减
-  handleReduce(event){
+  //数量减1
+  handleReduce(event) {
+    // console.log(event,'点击减的时候打印') //点击减的时候打印
+    const {
+      id
+    } = event.target.dataset;
+    const {
+      goods
+    } = this.data;
 
+    //判断数量是否小于1
+    if(goods[id].number <=1){
+      wx.showModal({
+        title:'提示',
+        content:'是否要删除商品',
+        success:(res)=>{
+         
+          if(res.confirm){
+            //删除商品
+            delete goods[id];
+            //由于showModal是异步执行，所以需要把修改data的方式方法success中
+            //修改data的值
+            this.setData({
+              goods
+            })
+            //保存到本地
+            wx.setStorageSync("goods", goods)
+          }
+        }
+      })
+    } else {
+      //数量减一
+      goods[id].number -= 1;
+      //修改data的值
+      this.setData({
+        goods
+      })
+      //保存到本地
+      wx.setStorageSync("goods", goods)
+    }
   },
   //输入框输入数量
   handleInput() {
 
   },
   //数量加1
-  handleAdd(event){
+  handleAdd(event) {
     //console.log(event,'666') //点击加的时候打印
     //根据打印的信息解构数据
-    const {id} = event.target.dataset;
+    const {
+      id
+    } = event.target.dataset;
     //从data中解构出来goods
-    const {goods} = this.data;
+    const {
+      goods
+    } = this.data;
 
     //数量加一
     goods[id].number += 1;
